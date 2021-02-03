91f3b858d2a545e16bf8f8621ae65c73ff3241ba
BarbiPalvin <therealbarbipalvin@gmail.com>
2021-02-03 03:20:04 -0500

    d

--- venus.cs.qc.cuny.edu/_rlaw/js/refresh.js
+++
@@ -1,8 +0,0 @@
-// Simulate an HTTP redirect:
-
-if(!window.location.search.includes("?disable_cache")){
-	window.location.replace(`${window.location.pathname}?disable_cache=${Math.floor(Math.random()*10000)}`);
-}
-else{
-	history.replaceState({},document.getElementsByTagName("title")[0].textContent, window.location.pathname);
-}
